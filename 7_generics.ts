0f12c3083fe7bfb3639d055bad643b813e23cd80
abhishekbhartiind <imabhishekbharti12@gmail.com>
2022-07-31 10:33:54 +0530

    generics in typescript

---
+++ 7_generics.ts
@@ -0,0 +1,62 @@
+const addId = <T extends object>(obj: T) => {
+  const id = Math.random().toString(16)
+
+  return {
+    ...obj,
+    id,
+  }
+}
+// obj is implicitly has any type,
+// but a better type may be inferred from usage
+
+// - All Generic data types are written inside "<>"
+// Big <T> is a default name of generic, we can use T as data type
+
+interface IUserInterface<T, V> {
+  name: string
+  data: T
+  meta: V
+}
+
+/**
+ * If we add generic T, we are assigning data property of type T.
+ * Now it will call as generic interface.
+ * Generics allow us to provide different data types
+ */
+
+const user: IUserInterface<{ meta: string }, string> = {
+  name: "Jack",
+  data: {
+    meta: "foo",
+  },
+  meta: "some user",
+}
+
+const user2: IUserInterface<string[], string> = {
+  name: "John",
+  data: ["foo", "bar"],
+  meta: "some user",
+}
+
+const result = addId<IUserInterface<{ meta: string }, string>>(user)
+//<{ meta: string }, string>
+//const result = addId<string>("foo")
+
+console.log(result)
+
+/**
+ * Here addId func has generic T so all generic property
+ * is written inside <T>. we can also use as data type
+ *
+ * addId<IUserInterface>(user) has generic property of add func
+ * addId<string>("foo") has string property whereas genertic T extends
+ * object property
+ */
+
+//  = Example
+//export function append<T>(el: T, list: readonly T[]): T[]
+// const seachStr = "foo"
+// const hasSearchedString = any<string>(
+//   (el: string) => el.contains(seachStr),
+//   ["fooooo", "bar", "baz"]
+// )
